7746cf4e7197c56308da294dff1ef5eb07e64360
ranimiftah12345678 <ranimiptahhh@gmail.com>
2024-10-25 05:33:47 +0700

    rata

--- RataNilai.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class RataNilai {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-         
-    }
-}
---
+++ RataNilai25.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+    public class RataNilai25 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int i, j;
+    float nilaiMhs, totalNilai, rataNilai;
+        i=1;
+    while (i<=5) {
+        totalNilai=0;
+        System.out.println("Input nilaimahasiswa ke " + i);
+        for(j=1;j<=5;j++){
+        System.out.print("Nilai ke- " + j + " = ");
+        nilaiMhs=sc.nextInt();
+        totalNilai+=nilaiMhs;
+        }
+    rataNilai=totalNilai/5;
+    System.out.println("rata rata nilai mahasiswa ke " + i + " adalah " + rataNilai);
+    i++;
+    }
+    sc.close();
+    }
+}
